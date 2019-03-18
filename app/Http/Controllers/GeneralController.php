479031976e074c037ea32b7713af8c125640d0ea
DenoGeek <kariukidennisde@gmail.com>
2019-03-18 20:59:36 +0300

    the update to orders migration

--- app/Http/Controllers/GeneralController.php
+++ app/Http/Controllers/GeneralController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Attachment;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Session;
 
 class GeneralController extends Controller
 {
@@ -10,7 +12,9 @@ class GeneralController extends Controller
 
     public function sessionFiles(Request $request)
     {
-
+        $docs = Session::get('upload_files');
+        $documents = Attachment::whereIn('id',$docs)->get();
+        return response()->json($documents);
     }
 
     public function uploadOrderFiles()

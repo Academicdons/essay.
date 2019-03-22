fa6fe2bb7da5c622e382be2c62d9b5161cf04bf9
DavidzMwangi <mwangidavidwanjohi@gmail.com>
2019-03-22 13:19:46 +0300

    thunder pusher addition

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Jobs\ThunderPushAsync;
 use App\Models\Order;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
 use Illuminate\Http\Request;
@@ -46,6 +47,12 @@ class LoginController extends Controller
           $order=Order::find($order_id);
           $order->created_by=$user->id;
           $order->save();
+
+
+          dispatch(new ThunderPushAsync($order->id,$event = ["event"=>"order",
+              "data"=>null
+          ]));
+
       }
      return redirect($this->redirectTo);
 

84f28bc0afcaa6ea1bf7223456e9b24cab55b3ca
DavidzMwangi <mwangidavidwanjohi@gmail.com>
2019-04-02 21:21:38 +0300

    changes on the whatsapp

--- resources/views/layouts/index.blade.php
+++ resources/views/layouts/index.blade.php
@@ -375,7 +375,7 @@
         </div>
     </footer>
 
-    <a href="https://api.whatsapp.com/send?phone=+1(919) 588 4106&text=Hola%21%20Quisiera%20m%C3%A1s%20informaci%C3%B3n%20sobre%20Varela%202." class="whatsapp-float" target="_blank">
+    <a href="https://api.whatsapp.com/send?phone=+1(919) 588 4106&text=Hello, chat with us." class="whatsapp-float" target="_blank">
         <i class="fa fa-whatsapp my-float"></i>
     </a>
 
